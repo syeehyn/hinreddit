70529112648ec1af88c2ceb4b5c2a2b818c6e551
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-04-01 00:21:33 -0700

    update first raw data

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -14,6 +14,6 @@ def fetch_posts(num, subreddit):
         posts.append([post.id, post.author,post.title, post.score, 
         post.subreddit, post.shortlink, post.num_comments, 
         post.selftext, post.created_utc, int(post.over_18), post.url])
-    posts = pd.DataFrame(posts,columns=['id', 'author_name','title', 'score', 'subreddit', \
-                                        'url', 'num_comments', 'body', 'created', 'over_18', 'content_url'])
+    posts = pd.DataFrame(posts,columns=['post_id', 'author_name','title', 'score', 'subreddit', \
+                                        'post_link', 'num_comments', 'body', 'created', 'over_18', 'content_url'])
     return posts
