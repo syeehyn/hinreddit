15ec53567c274d86c6c72e2bc97c7c7bc4062b3a
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-04-10 00:08:22 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 import os
-import praw as pr
+# import praw as pr
 ROOT_DIR = Path(__file__).parent.parent
 CLIENTID = json.load(open(os.path.join(ROOT_DIR, 'config/credentials.json')))['client_id']
 CLIENTSECRET = json.load(open(os.path.join(ROOT_DIR, 'config/credentials.json')))['client_secret']
