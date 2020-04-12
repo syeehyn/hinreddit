1e02c23484e933134f6e7fd6a598a39c0d29afff
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-04-12 01:44:26 -0700

    update first checkpoint

--- run.py
+++ run.py
@@ -8,7 +8,11 @@ import pandas as pd
 from p_tqdm import p_map
 
 TESTPARAMS = json.load(open('config/test-params.json'))
-TESTDIR = os.path.join('./tests')
+EDAPARAMS = json.load(open('config/eda-params.json'))
+DATAPARAMS = json.load(open('config/data-params.json'))
+TESTDIR = TESTPARAMS['META_ARGS']['filepath']
+EDADIR = EDAPARAMS['META_ARGS']['filepath']
+DATADIR = DATAPARAMS['META_ARGS']['filepath']
 def env_test():
     if not os.path.exists(TESTDIR):
         os.mkdir(TESTDIR)
@@ -21,25 +25,50 @@ def env_test():
     if not os.path.exists(os.path.join(TESTDIR, 'raw', 'comments')):
         os.mkdir(os.path.join(TESTDIR, 'raw', 'comments'))
     return
+def env_eda():
+    if not os.path.exists(EDADIR):
+        os.mkdir(EDADIR)
+    if not os.path.exists(os.path.join(EDADIR, 'raw')):
+        os.mkdir(os.path.join(EDADIR, 'raw'))
+    if not os.path.exists(os.path.join(EDADIR, 'raw', 'posts')):
+        os.mkdir(os.path.join(EDADIR, 'raw', 'posts'))
+    if not os.path.exists(os.path.join(EDADIR, 'raw', 'posts_detail')):
+        os.mkdir(os.path.join(EDADIR, 'raw', 'posts_detail'))
+    if not os.path.exists(os.path.join(EDADIR, 'raw', 'comments')):
+        os.mkdir(os.path.join(EDADIR, 'raw', 'comments'))
+    return
+def env_data():
+    if not os.path.exists(DATADIR):
+        os.mkdir(DATADIR)
+    if not os.path.exists(os.path.join(DATADIR, 'raw')):
+        os.mkdir(os.path.join(DATADIR, 'raw'))
+    if not os.path.exists(os.path.join(DATADIR, 'raw', 'posts')):
+        os.mkdir(os.path.join(DATADIR, 'raw', 'posts'))
+    if not os.path.exists(os.path.join(DATADIR, 'raw', 'posts_detail')):
+        os.mkdir(os.path.join(DATADIR, 'raw', 'posts_detail'))
+    if not os.path.exists(os.path.join(DATADIR, 'raw', 'comments')):
+        os.mkdir(os.path.join(DATADIR, 'raw', 'comments'))
+    return
+
 def main(targets):
     if any(['test'in i for i in targets]):
         env_test()
+    if any(['eda'in i for i in targets]):
+        env_eda()
+    if any(['real'in i for i in targets]):
+        env_data()
     if 'data-test' in targets:
         fetch_submissions(**TESTPARAMS)
-        print('post fetched')
         submissions_detail(TESTDIR)
-        print('post details fetched')
         comments_detail(TESTDIR)
-        print('all fetched')
-    if 'post-test' in targets:
-        fetch_submissions(**TESTPARAMS)
-        return 'Done'
-    if 'comment-test' in targets:
-        submissions_detail(TESTDIR)
-        return 'Done'
-    if 'comment-detail-test' in targets:
-        comments_detail(TESTDIR)
-        return  'Done'
+    if 'data-eda' in targets:
+        fetch_submissions(**EDAPARAMS)
+        submissions_detail(EDADIR)
+        comments_detail(EDADIR)
+    if 'data-real' in targets:
+        fetch_submissions(**DATAPARAMS)
+        submissions_detail(DATADIR)
+        comments_detail(DATADIR)
 
 
 if __name__ == '__main__':
