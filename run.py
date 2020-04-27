095538d42d2c520294cc10ad163bba79a46a649c
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-04-26 18:35:35 -0700

    upload labeling

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 import sys
-from src.etl import fetch_submissions, submissions_detail, comments_detail
+from src.etl import *
 from src.embedding import construct_matrices
 from src.utils import evaluate
 from src.models import *
@@ -51,23 +51,30 @@ def env_data():
 def main(targets):
     if any(['test'in i for i in targets]):
         env_test()
-    if any(['real'in i for i in targets]):
+    else:
         env_data()
     if 'data-test' in targets:
         fetch_submissions(**TESTPARAMS)
         submissions_detail(TESTDIR)
         comments_detail(TESTDIR)
-    if 'data-real' in targets:
+    if 'data' in targets:
         fetch_submissions(**DATAPARAMS)
         submissions_detail(DATADIR)
         comments_detail(DATADIR)
-    if 'embedding-test' in targets:
-        construct_matrices(DATADIR)
-    if 'embedding-real' in targets:
-        construct_matrices(DATADIR)
-    if 'evaluate-real' in targets:
-        res = evaluate(.2, 'hinmodel', DATADIR)
-        print(res)
+    if 'label' in targets:
+        post_path = os.path.join(DATADIR, 'raw/posts')
+        label_path = os.path.join(DATADIR, 'interim/labels')
+        train_path = params['train_data']
+        word_vector = params['word_vector']
+        tokenizer, model = train_model(train_path, word_vector)
+        label_posts(post_path, tokenizer, model, label_path)
+    if 'label-test' in targets:
+        post_path = os.path.join(TESTDIR, 'raw/posts')
+        label_path = os.path.join(TESTDIR, 'interim/labels')
+        train_path = params['train_data']
+        word_vector = params['word_vector']
+        tokenizer, model = train_model(train_path)
+        label_posts(post_path, tokenizer, model)
     if 'baseline' in targets:
         post_path = os.path.join(DATADIR, 'raw/posts')
         label_path = os.path.join(DATADIR, 'interim/labels')
@@ -82,6 +89,13 @@ def main(targets):
         labels = get_csvs(label_path)[['id','label']]
         df = extract_feat(posts, labels)
         baseline_model(df)
+    # if 'embedding-test' in targets:
+    #     construct_matrices(DATADIR)
+    # if 'embedding-real' in targets:
+    #     construct_matrices(DATADIR)
+    # if 'evaluate-real' in targets:
+    #     res = evaluate(.2, 'hinmodel', DATADIR)
+    #     print(res)
 
 
 if __name__ == '__main__':
--- src/etl/__init__.py
+++ src/etl/__init__.py
@@ -1,1 +1,2 @@
 from .etl import fetch_submissions, submissions_detail, comments_detail
+from .label import label_post, train_model
