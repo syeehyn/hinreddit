8c85b1d7c4c383ebe8a4536bd3201439cae4370c
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-05-08 21:30:04 -0700

    Update label.py

--- src/etl/label.py
+++ src/etl/label.py
@@ -92,10 +92,9 @@ def label_posts(path, model, tokenizer, thres = 0.5, maxlen = 200):
     outpath = os.path.join(path, 'interim', 'label', 'post')
     posts = pd.concat([pd.read_csv(i, usecols = ['id', 'selftext']) for i in glob.glob(post_path)], ignore_index=True)
     valid_posts = posts[(posts.selftext.notnull())&(posts.selftext!='[deleted]')&(posts.selftext!= '[removed]')]
-    print(valid_posts['id'].isnull().mean())
     predictions = label(valid_posts.selftext.apply(preprocess_text), tokenizer, model, maxlen)
     valid_predictions = pd.DataFrame(predictions, columns=["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"])
-    valid_predictions['post_id'] = valid_posts['id']
+    valid_predictions['post_id'] = valid_posts['id'].copy().values
     valid_predictions = valid_predictions[['post_id', "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]]
     #valid_predictions.hist(bins = 20)
     deleted_posts = posts[(posts.selftext=='[deleted]')|(posts.selftext=='[removed]')][['id']]
