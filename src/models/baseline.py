b8cd8b2e007a032662ce4858474ebe0aedee4bbd
Yu-Chun Chen <yuc330@ucsd.edu>
2020-05-12 01:37:16 -0700

    update baseline model

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -12,27 +12,44 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, auc, roc_curve
 from sklearn.model_selection import train_test_split
 
-def get_csvs(path):
-    posts = glob.glob(path+'*.csv')
+import warnings
+warnings.filterwarnings("ignore")
+
+def get_csvs(dir_path):
+    posts = glob.glob(dir_path+'*.csv')
     return pd.concat([pd.read_csv(i) for i in posts], ignore_index = True)
 
+def clean_tweet(tweet): 
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split()) 
 
-def extract_feat(posts, labels):
-    posts= posts.merge(labels, on='id')
+def sensitive_word(text):
+    sen_lst = ['fuck', 'nigger', 'fucking', 'shit', 'sex', 'ass', 'asshole']
+    lst = text.split()
+    if any(word in lst for word in sen_lst):
+        return 1
+    else:
+        return 0
 
-    posts['length']=posts.selftext.str.split().str.len()
+def extract_feat(p_dir, l_path):
+    
+    posts = get_csvs(p_dir)
+    labels = pd.read_csv(l_path)
+    post_labeldf = pd.merge(labels,posts,left_on = 'post_id',right_on = 'id',how = 'inner').drop_duplicates()
+    hate_benign_post = post_labeldf[(post_labeldf.label == 1) | (post_labeldf.label == 0)]
+    hate_benign_post['selftext'] = hate_benign_post.selftext.fillna("")
+    
+    hate_benign_post['selftext'] = hate_benign_post['selftext'].apply(clean_tweet)
+    hate_benign_post['len_content'] = hate_benign_post.selftext.apply(lambda x:len(x.split(" ")))
+    hate_benign_post['sensitive'] = hate_benign_post.selftext.apply(sensitive_word)
 
-    posts['title_length']=posts.title.str.split().str.len()
 
-    posts = posts[~(posts['label'].isna())&~(posts['label']==-1.0)]
+    hate_benign_post['label']=hate_benign_post['label'].astype(int)
+    return hate_benign_post[['num_comments', 'subreddit', 'score', 'len_content', 'sensitive', 'label']]
 
-    posts['label']=posts['label'].astype(int)
-    return posts[['num_comments', 'subreddit', 'score', 'length', 'title_length', 'label']]
-
-def preprocess(X):
+def preprocess(X, cat_feat = ['subreddit', 'sensitive'], num_feat = ['num_comments','len_content','score']):
     """
     provide the column transformer for the dataframe of simple features
     
@@ -40,15 +57,19 @@ def preprocess(X):
         X - dataframe of the apps w/ simple feature to create column transformer
         
     """
-    cat_feat = ['subreddit']
     cat_trans = Pipeline(steps=[
         ('onehot', OneHotEncoder(handle_unknown = 'ignore'))
         ])
 
     num_trans = Pipeline(steps = [('standard_scalar',StandardScaler())])
-    num_feat = ['num_comments','length', 'title_length','score']
 
     return ColumnTransformer(transformers=[('cat', cat_trans,cat_feat), ('num', num_trans, num_feat)])
+
+def calc_auc(model, X_test, y_test):
+    probs = model.predict_proba(X_test)
+    preds = probs[:,1]
+    fpr, tpr, threshold = roc_curve(y_test, preds)
+    return auc(fpr, tpr)
 
 def result_LR(df_train, df_test, pre, y_column = 'label'):
     """
@@ -64,14 +85,15 @@ def result_LR(df_train, df_test, pre, y_column = 'label'):
     X = df_train.drop(y_column, 1)
     y = df_train[y_column]
     pipe = Pipeline(steps=[('preprocessor', pre),
-                       ('clf', LogisticRegression())
+                       ('clf', LogisticRegression(class_weight='balanced'))
                        ])
     pipe.fit(X,y)
     X_te = df_test.drop(y_column, 1)
     y_te = df_test[y_column]
     y_pred = pipe.predict(X_te)
     tn, fp, fn, tp = confusion_matrix(y_te, y_pred).ravel()
-    return tn, fp, fn, tp
+    auc = calc_auc(pipe, X_te, y_te)
+    return [tn, fp, fn, tp, auc]
 
 def result_RF(df_train, df_test, pre, y_column = 'label'):
     """
@@ -87,14 +109,15 @@ def result_RF(df_train, df_test, pre, y_column = 'label'):
     X = df_train.drop(y_column, 1)
     y = df_train[y_column]
     pipe = Pipeline(steps=[('preprocessor', pre),
-                       ('clf', RandomForestClassifier(max_depth=2, random_state=0))
+                       ('clf', RandomForestClassifier(max_depth=2, random_state=0, class_weight='balanced'))
                        ])
     pipe.fit(X,y)
     X_te = df_test.drop(y_column, 1)
     y_te = df_test[y_column]
     y_pred = pipe.predict(X_te)
     tn, fp, fn, tp = confusion_matrix(y_te, y_pred).ravel()
-    return tn, fp, fn, tp
+    auc = calc_auc(pipe, X_te, y_te)
+    return [tn, fp, fn, tp, auc]
 
 def result_GBT(df_train, df_test, pre, y_column = 'label'):
     """
@@ -117,16 +140,18 @@ def result_GBT(df_train, df_test, pre, y_column = 'label'):
     y_te = df_test[y_column]
     y_pred = pipe.predict(X_te)
     tn, fp, fn, tp = confusion_matrix(y_te, y_pred).ravel()
-    return tn, fp, fn, tp
+    auc = calc_auc(pipe, X_te, y_te)
+    return [tn, fp, fn, tp, auc]
 def compute_metrics(mat):
     """
-    output metrics including 'tn', 'fp', 'fn', 'tp', 'acc', 'fnr'
+    output metrics including precision, recall, and AUC
 
     Args:
        mat - confustion matrix 
         
     """
-    return mat + [(mat[0]+mat[3])/sum(mat), mat[2]/(mat[2]+mat[3])]#include confusion matrix and acc, fpr
+    tn, fp, fn, tp = mat[0], mat[1], mat[2], mat[3]
+    return tp/(tp+fp), tp/(tp+fn), mat[4]
 
 def save_baseline_result(lr, rf, gbt):
     """
@@ -138,8 +163,9 @@ def save_baseline_result(lr, rf, gbt):
         gbt - test result of gradient boost classifier
         
     """
-    baseline_result = pd.DataFrame([lr, rf, gbt], columns=['tn', 'fp', 'fn', 'tp', 'acc', 'fnr'], index = np.array(['logistic regression', 'random forest', 'gradient boost']))
-    baseline_result.to_csv(os.path.join('output', 'baseline_result.csv'))
+    baseline_result = pd.DataFrame([lr, rf, gbt], columns=['precision', 'recall', 'AUC'], index = np.array(['logistic regression', 'random forest', 'gradient boost']))
+    #baseline_result.to_csv(os.path.join('output', 'baseline_result.csv'))
+    return baseline_result
 def baseline_model(df, y_col = 'label', test_size=0.3):
     """
     the whole process of training baseline model to saveing the result to file
@@ -162,16 +188,15 @@ def baseline_model(df, y_col = 'label', test_size=0.3):
     
     print('start training baseline models...')
     result_lr = result_LR(df_train, df_test, pre)
-    lr = compute_metrics(list(result_lr))
+    lr = compute_metrics(result_lr)
 
     result_rf = result_RF(df_train, df_test, pre)
-    rf = compute_metrics(list(result_rf))
+    rf = compute_metrics(result_rf)
 
     result_gbt = result_GBT(df_train, df_test, pre)
-    gbt = compute_metrics(list(result_gbt))
+    gbt = compute_metrics(result_gbt)
     print('finish training baseline models')
 
-    baseline_result = pd.DataFrame([lr, rf, gbt], columns=['tn', 'fp', 'fn', 'tp', 'acc', 'fnr'], index = np.array(['logistic regression', 'random forest', 'gradient boost']))
-    save_baseline_result(lr, rf, gbt)
+    baseline_result = save_baseline_result(lr, rf, gbt)
     print('baseline test result saved to output directory')
     return baseline_result
