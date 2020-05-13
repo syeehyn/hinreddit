0a3d2318ea8752cfa6cd29bd77ee3fa6c6e34ff7
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-05-13 03:28:31 -0700

    Update graph.py

--- src/embedding/graph.py
+++ src/embedding/graph.py
@@ -26,19 +26,19 @@ def create_graph(fp):
     labl = labl[labl.label != -1]
     post.author = post.author.str.lower()
     post.subreddit = post.subreddit.str.lower()
-    # post = post[(post.author != '[deleted]')&(post.author != 'automoderator')& (post.author != 'snapshillbot')]
+    post = post[(post.author != '[deleted]')&(post.author != 'automoderator')& (post.author != 'snapshillbot')]
     comm['parent_id'] = comm.parent_id.str[3:]
     comm = comm[['id','author', 'is_submitter', 'parent_id']]
     comm.columns = ['comment_id','author', 'is_submitter', 'parent_id']
     comm.author = comm.author.str.lower()
-    # comm = comm[(comm.author != '[deleted]')&(comm.author != 'automoderator') & (comm.author != 'snapshillbot')]
+    comm = comm[(comm.author != '[deleted]')&(comm.author != 'automoderator') & (comm.author != 'snapshillbot')]
     comm = comm.dropna()
     comm = comm[(comm.parent_id.isin(post.id)) | (comm.parent_id.isin(comm.comment_id))]
     post = post[(post.id.isin(labl.post_id)) & (post.id.isin(comm.parent_id))]
-    # author_counts = comm.author.value_counts()
-    # author_mask = author_counts > 3
-    # author_counts = author_counts[author_mask].index
-    # comm = comm[comm.author.isin(author_counts)]
+    author_counts = comm.author.value_counts()
+    author_mask = author_counts > 3
+    author_counts = author_counts[author_mask].index
+    comm = comm[comm.author.isin(author_counts)]
     post = post[post.id.isin(comm.parent_id)]
     comm_root = comm[comm.parent_id.str.len() == 6]
     comm_nest = comm[comm.parent_id.str.len() == 7]
