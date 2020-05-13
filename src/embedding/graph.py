1c7c7e379f1e1e1c8c461fbfd16414a5a8bd49ce
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-05-13 00:04:56 -0700

    Update graph.py

--- src/embedding/graph.py
+++ src/embedding/graph.py
@@ -3,6 +3,7 @@ from glob import glob
 import os.path as osp
 import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
 import json
 from scipy import sparse
 
@@ -12,10 +13,11 @@ POST_DIR = osp.join('raw', 'posts', '*.csv')
 OUT_DIR = osp.join('interim', 'graph')
 
 def create_graph(fp):
+    print('starting preprocessing: (filtering)')
     comm = osp.join(fp, COMM_DIR)
     post = osp.join(fp, POST_DIR)
     labl = osp.join(fp, LABL_DIR)
-    comm = pd.concat([pd.read_csv(i, usecols = ['author', 'link_id', 'subreddit']
+    comm = pd.concat([pd.read_csv(i, usecols = ['id', 'author', 'is_submitter', 'parent_id']
                         ) for i in glob(comm)])
     post = pd.concat([pd.read_csv(i, usecols = ['id', 'author', 'subreddit']
                         ) for i in glob(post)])
@@ -25,63 +27,59 @@ def create_graph(fp):
     post.subreddit = post.subreddit.str.lower()
     post = post[(post.author != '[deleted]')&(post.author != 'automoderator')& (post.author != 'snapshillbot')]
     post = post[post.id.isin(labl.post_id)]
-    comm['post_id'] = comm.link_id.str[3:]
-    comm = comm[['author', 'subreddit', 'post_id']]
+    comm['parent_id'] = comm.parent_id.str[3:]
+    comm = comm[['id','author', 'is_submitter', 'parent_id']]
+    comm.columns = ['comment_id','author', 'is_submitter', 'parent_id']
     comm.author = comm.author.str.lower()
-    comm.subreddit = comm.subreddit.str.lower()
     comm = comm[(comm.author != '[deleted]')&(comm.author != 'automoderator') & (comm.author != 'snapshillbot')]
-    author_counts = comm.author.value_counts()
-    author_mask = author_counts > 3
-    author_counts = author_counts[author_mask].index
-    comm = comm[comm.author.isin(author_counts)]
-    comm = comm[comm.post_id.isin(post.id)]
-    add_comm = post.copy()
-    add_comm.columns = ['post_id', 'author', 'subreddit']
-    add_comm =add_comm[['author', 'subreddit', 'post_id']].drop_duplicates()
-    comm = pd.concat([comm, add_comm], ignore_index = True).drop_duplicates()
-    post.columns = ['node_id', 'author', 'subreddit']
-    post = post.drop_duplicates()
-    comm.columns = ['node_id', 'subreddit', 'parent_id']
-    comm['is_post'] = False
-    post['is_post'] = True
-    graph = pd.concat([
-                post[['node_id', 'is_post']],
-                comm[['node_id', 'is_post']]
-                ]).drop_duplicates().reset_index(drop = True)
-    graph = pd.merge(graph, labl, left_on = 'node_id', right_on = 'post_id', how = 'left')
-    graph = graph[['node_id', 'is_post', 'label']]
-    graph = graph.fillna(-1)
+    comm = comm.dropna()
+    comm = comm[(comm.parent_id.isin(post.id)) | (comm.parent_id.isin(comm.comment_id))]
+    post = post[post.id.isin(comm.parent_id)]
+    comm_root = comm[comm.parent_id.str.len() == 6]
+    comm_nest = comm[comm.parent_id.str.len() == 7]
+    comm_ = comm_root[['comment_id', 'author']]
+    print('starting preprocessing: (edges)')
+    comm_nest_edges = pd.merge(comm_nest, comm_, left_on = 'parent_id', right_on = 'comment_id', how = 'inner')[['author_x', 'author_y']]
+    comm_nest_edges.columns = ['who', 'whom']
+    comm_root_edges_lower = pd.merge(comm_root, post, left_on = 'parent_id', right_on = 'id', how = 'inner')[['author_x', 'id']]
+    comm_root_edges_lower.columns = ['who', 'whom']
+    comm_root_edges_upper = pd.merge(comm_root, post, left_on = 'parent_id', right_on = 'id', how = 'inner')[['id', 'author_x']]
+    comm_root_edges_upper.columns = ['whom', 'who']
+    comm_root_edges = pd.concat([comm_root_edges_lower, comm_root_edges_upper], ignore_index = True)
+    edges = pd.concat([comm_nest_edges, comm_root_edges], ignore_index = True)
+    graph_idx = np.unique(np.append(edges.who.values, edges.whom.values)).reshape(-1, 1)
     encoder = OrdinalEncoder()
-    graph.node_id = encoder.fit_transform(graph[['node_id']])
-    post.node_id = encoder.transform(post[['node_id']])
-    comm.node_id = encoder.transform(comm[['node_id']])
-    comm.parent_id = encoder.transform(comm[['parent_id']])
-    graph = graph.sort_values('node_id')
-    edge_index = comm[['node_id', 'parent_id']].drop_duplicates().sort_values('node_id')
-    graph.to_csv(osp.join(fp, OUT_DIR, 'nodes.csv'), header = True, index = False)
-    with open(osp.join(fp, OUT_DIR, 'nodes_info.json'), 'w') as f:
-        json.dump(pd.Series(encoder.categories_[0]).to_dict(), f)
-    post = graph[graph.is_post.astype(bool)]
-    user = graph[~graph.is_post.astype(bool)]
-    post_id = post.node_id.values
-    user_id = user.node_id.values
-    post_map = pd.DataFrame(
-            {
-                'parent_id': post_id,
-                'post': np.arange(len(post_id))
-            }
+    encoder.fit(graph_idx)
+    edges['who_id'] = encoder.transform(edges.who.values.reshape(-1, 1))
+    edges['whom_id'] = encoder.transform(edges.whom.values.reshape(-1, 1))
+    edge_pairs = edges['who_id'].astype(int).astype(str) + edges['whom_id'].astype(int).astype(str)
+    pari_counts = edge_pairs.value_counts().to_frame()
+    edge_pairs = edge_pairs.to_frame()
+    edge_weights = pd.merge(edge_pairs, pari_counts, left_on = 0, right_index = True, how = 'left')['0_y'].values
+    edges['weights'] = (edge_weights - edge_weights.mean()) / edge_weights.std()
+    edges_ = edges[['who_id', 'whom_id', 'weights']].drop_duplicates().sort_values(['who_id', 'whom_id'])
+    edge_idx = edges_[['who_id', 'whom_id']].values.astype(int)
+    edge_weight = edges_[['weights']].values
+    post_label = pd.merge(post, labl, left_on = 'id', right_on = 'post_id', how = 'left')
+    print('starting preprocessing: (nodes)')
+    nodes = pd.DataFrame(
+        {
+            'node_name': pd.Series(encoder.categories_[0])
+        }
     )
-    user_map = pd.DataFrame(
-            {
-                'node_id': user_id,
-                'user': np.arange(len(user_id))
-            }
-    )
-    mat_indx = edge_index.copy()
-    mat_indx = pd.merge(mat_indx, user_map, on = 'node_id', how = 'left')
-    mat_indx = pd.merge(mat_indx, post_map, on = 'parent_id', how = 'left')
-    mat_indx = mat_indx[['post', 'user']].sort_values(['post', 'user'])
-    mat_indx = mat_indx.values
-    adj_matrix = sparse.csc_matrix((np.ones(mat_indx.shape[0]), (mat_indx[:, 0], mat_indx[:, 1])))
-    sparse.save_npz(osp.join(fp, OUT_DIR, 'adj_matrix.npz'), adj_matrix)
-    edge_index.to_csv(osp.join(fp, OUT_DIR, 'edges.csv'), header = True, index = False)
+    nodes = pd.merge(nodes, post_label, left_on = 'node_name', right_on = 'id', how = 'left')
+    nodes = pd.merge(nodes, comm, left_on = 'node_name', right_on = 'author', how = 'left')
+    nodes = nodes[['node_name', 'subreddit', 'is_submitter','label']]
+    nodes.subreddit = nodes.subreddit.fillna('0')
+    nodes.label = nodes.label.fillna(-1)
+    nodes.is_submitter = nodes.is_submitter.fillna(True).astype(int)
+    X = OneHotEncoder(sparse = False).fit_transform(nodes.subreddit.values.reshape(-1, 1))
+    X = np.hstack([X, nodes.is_submitter.values.reshape(-1, 1)])
+    post_mask = (y != -1).reshape(-1, 1)
+    y = nodes.label.values.reshape(-1, 1)
+    node_output = np.hstack([X, post_mask, y])
+    edge_output = np.hstack([edge_idx, edge_weight])
+    print('writing raw graphs')
+    np.save(osp.join(fp, OUT_DIR, 'nodes.npy'), node_output)
+    np.save(osp.join(fp, OUT_DIR, 'edges.npy'), edge_output)
+
