07988f1f1298c31fc83ab42dcbdce4749d2c69fc
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-05-13 03:54:07 -0700

    Update graph.py

--- src/embedding/graph.py
+++ src/embedding/graph.py
@@ -18,7 +18,7 @@ def create_graph(fp):
     comm = osp.join(fp, COMM_DIR)
     post = osp.join(fp, POST_DIR)
     labl = osp.join(fp, LABL_DIR)
-    comm = pd.concat([pd.read_csv(i, usecols = ['id', 'author', 'is_submitter', 'lind_id']
+    comm = pd.concat([pd.read_csv(i, usecols = ['id', 'author', 'is_submitter', 'link_id']
                         ) for i in glob(comm)])
     post = pd.concat([pd.read_csv(i, usecols = ['id', 'author', 'subreddit']
                         ) for i in glob(post)])
@@ -27,24 +27,24 @@ def create_graph(fp):
     post.author = post.author.str.lower()
     post.subreddit = post.subreddit.str.lower()
     post = post[(post.author != '[deleted]')&(post.author != 'automoderator')& (post.author != 'snapshillbot')]
-    comm['lind_id'] = comm.lind_id.str[3:]
-    comm = comm[['id','author', 'is_submitter', 'lind_id']]
-    comm.columns = ['comment_id','author', 'is_submitter', 'lind_id']
+    comm['link_id'] = comm.link_id.str[3:]
+    comm = comm[['id','author', 'is_submitter', 'link_id']]
+    comm.columns = ['comment_id','author', 'is_submitter', 'link_id']
     comm.author = comm.author.str.lower()
     comm = comm[(comm.author != '[deleted]')&(comm.author != 'automoderator') & (comm.author != 'snapshillbot')]
     comm = comm.dropna()
-    comm = comm[(comm.lind_id.isin(post.id)) | (comm.lind_id.isin(comm.comment_id))]
-    post = post[(post.id.isin(labl.post_id)) & (post.id.isin(comm.lind_id))]
+    comm = comm[(comm.link_id.isin(post.id)) | (comm.link_id.isin(comm.comment_id))]
+    post = post[(post.id.isin(labl.post_id)) & (post.id.isin(comm.link_id))]
     author_counts = comm.author.value_counts()
     author_mask = author_counts > 3
     author_counts = author_counts[author_mask].index
     comm = comm[comm.author.isin(author_counts)]
-    post = post[post.id.isin(comm.lind_id)]
+    post = post[post.id.isin(comm.link_id)]
     comm_root = comm.copy()
     print('start preprocessing: (edges)')
-    comm_root_edges_lower = pd.merge(comm_root, post, left_on = 'lind_id', right_on = 'id', how = 'inner')[['author_x', 'id']]
+    comm_root_edges_lower = pd.merge(comm_root, post, left_on = 'link_id', right_on = 'id', how = 'inner')[['author_x', 'id']]
     comm_root_edges_lower.columns = ['who', 'whom']
-    comm_root_edges_upper = pd.merge(comm_root, post, left_on = 'lind_id', right_on = 'id', how = 'inner')[['id', 'author_x']]
+    comm_root_edges_upper = pd.merge(comm_root, post, left_on = 'link_id', right_on = 'id', how = 'inner')[['id', 'author_x']]
     comm_root_edges_upper.columns = ['who', 'whom']
     edges = pd.concat([comm_root_edges_lower, comm_root_edges_upper], ignore_index = True)
     graph_idx = np.unique(np.append(edges.who.values, edges.whom.values)).reshape(-1, 1)
