dcb1a68620a17f466551da2dc127e499c531433a
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-05-26 17:45:55 -0700

    Update node2vec.py

--- src/embedding/node2vec.py
+++ src/embedding/node2vec.py
@@ -16,28 +16,25 @@ def node2vec(fp, PARAMS):
     N = io.loadmat(osp.join(fp, 'interim', 'graph', PARAMS['GRAPH_NAME']))['N']
     edge_idx, x =from_scipy_sparse_matrix(N)
     post_indx = io.loadmat(osp.join(fp, 'interim', 'graph', PARAMS['GRAPH_NAME']))['post_indx']
-    post_indx = torch.from_numpy(post_indx.reshape(-1,))
+    post_indx = post_indx.reshape(-1,)
     data = Data(
         x = x,
         edge_index = edge_idx
     )
-    print(data.num_nodes)
-    print(data)
-    loader = DataLoader(post_indx, batch_size=PARAMS['BATCH_SIZE'], shuffle=False)
     if PARAMS['CUDA']:
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
     else:
         device = 'cpu'
     model = Node2Vec(data.edge_index, embedding_dim=PARAMS['EMBEDDING_DIM'], walk_length=PARAMS['WALK_LENGTH'],
-                    context_size=PARAMS['CONTEXT_SIZE'], walks_per_node=PARAMS['WALKS_PER_NODE'], p = PARAMS['P'], q = PARAMS['Q'], sparse=True)
-    model, data = model.to(device), data.to(device)
+                    context_size=PARAMS['CONTEXT_SIZE'], walks_per_node=PARAMS['WALKS_PER_NODE'], p = PARAMS['P'], q = PARAMS['Q'], sparse=True).to(device)
+    loader = model.loader(batch_size=PARAMS['BATCH_SIZE'], shuffle=True, num_workers=8)
     optimizer = torch.optim.SparseAdam(model.parameters(), lr=PARAMS['LEARNING_RATE'])
     def train():
         model.train()
         total_loss = 0
-        for subset in tqdm(loader):
+        for pow_rw, neg_rw in tqdm(loader):
             optimizer.zero_grad()
-            loss = model.loss(data.edge_index, subset.to(device))
+            loss = model.loss(pos_rw.to(device), neg_rw.to(device))
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
@@ -52,12 +49,12 @@ def node2vec(fp, PARAMS):
         print('Epoch: {:02d}, Node2vec Loss: {:.4f}'.format(epoch, loss))
     model.eval()
     with torch.no_grad():
-        z = model(post_indx.to(device))
+        z = model()
     if not os.path.exists(os.path.join(fp, 'processed', 'node2vec')):
         os.makedirs(os.path.join(fp, 'processed', 'node2vec'), exist_ok=True)
     with open(osp.join(fp, 'processed', 'node2vec', 'log_1.json'), 'w') as f:
         json.dump({'loss': losses}, f)
-    z = z.detach().cpu().numpy()
+    z = z.detach().cpu().numpy()[post_indx, :]
     np.save(osp.join(fp, 'processed', 'node2vec', PARAMS['EMBEDDING_NAME']), z)
     print('successfully saved embedding')
 
